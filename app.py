7e719e6cf7ac46edb5710084ba01cca4a75fce2c
Alfin Yusriansyah <alfinyusrian001@gmail.com>
2024-01-23 22:16:17 +0700

    add klasifikasi hujan

--- app.py
+++ app.py
@@ -19,18 +19,18 @@ def predict(model, input_data):
     return prediction[0]
 
 # Tampilkan judul aplikasi
-st.title('SapuJagad V1 Socket : 89')
-st.title('Aplikasi Prediksi Curah Hujan')
+st.title('SapuJagad V1 Socket : 89 ')
+st.subheader('Aplikasi prediksi curah hujan daerah Wamena, Cengkareng, Termindung')
 
 # Memuat model
 model = load_model()
 
 # 'suhutitikembun','suhu','evaporasi','kelembapan','district'
 # Memasukkan variabel input dari pengguna
-suhu_titik_embun = st.slider('Suhu Titik Embun', min_value=-10.0, max_value=30.0, value=15.0)
+suhu_titik_embun = st.slider('Suhu Titik Embun', min_value=0.00, max_value=30.0, value=15.0)
 kelembapan = st.slider('Kelembapan', min_value=0, max_value=100, value=50)
 suhu = st.slider('Suhu',min_value=-10.0, max_value=30.0, value=15.0)
-evaporasi = st.slider('Evaporasi',min_value=-10.0, max_value=30.0, value=15.0)
+evaporasi = st.slider('Evaporasi',min_value=0.00, max_value=30.0, value=15.0)
 
 if model is not None:
 # Tombol untuk melakukan prediksi
@@ -39,7 +39,20 @@ if model is not None:
         input_data = [suhu_titik_embun, kelembapan, suhu, evaporasi ]  # Tambahkan variabel input lainnya sesuai kebutuhan
         # Lakukan prediksi
         hasil_prediksi = predict(model, input_data)
-        # Tampilkan hasil prediksi
-        st.success(f'Hasil prediksi: {hasil_prediksi}')
+        if hasil_prediksi < 5 :
+            st.subheader("Hujan sangat ringan")
+            st.success(f'Hasil prediksi:  {hasil_prediksi}')
+        elif 5 <= hasil_prediksi <= 20 :
+            st.subheader("Hujan ringan")
+            st.success(f'Hasil prediksi:  {hasil_prediksi}')
+        elif 20 <= hasil_prediksi <= 50 : 
+            st.subheader("Hujan sedang")
+            st.success(f'Hasil prediksi:  {hasil_prediksi}')
+        elif 51 <= hasil_prediksi <= 100 : 
+            st.subheader("Hujan lebat")
+            st.success(f'Hasil prediksi:  {hasil_prediksi}')
+        else : 
+            st.subheader("Hujan sangat lebat")           
+            st.success(f'Hasil prediksi: {hasil_prediksi}')
 else :
     st.text("belum ada library")        
